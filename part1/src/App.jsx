4ff737d22fb604b40ae2d03e4f9fc6e3ae361720
elber01 <hamilton.edwin@gmail.com>
2024-09-07 11:43:16 -0400

    Exercises 1.3-1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,9 +6,9 @@ const Hello = (props) => {
   console.log(props)
   return (
     <div>
-      <p>
+    <p>
 
-        Hello {props.name}, you are {props.age} years old
+      Hello {props.name}, you are {props.age} years old
       </p>
     </div>
   )
@@ -19,14 +19,14 @@ const App = () => {
   const name = 'Peter'
   const age = 10
 
-  return (
-    <div>
-      <h1>Greetings</h1>
+ return (
+   <div>
+     <h1>Greetings</h1>
 
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+     <Hello name='Maya' age={26 + 10} />
+    <Hello name={name} age={age} />
     </div>
-  )
+ )
 }
 
 //este export es necesario para que la app funcione
